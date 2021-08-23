32cf495a828c68d6c535be62e37f137591d900c2
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-08-23 14:32:54 +1000

    Make the clipboard access more resilient

--- ClipboardRedux.cs
+++ ClipboardRedux.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Threading;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using IFormsDataObject = System.Windows.Forms.IDataObject;
+using System.Threading;
+using System.Windows.Forms;
 using FormsDataObject = System.Windows.Forms.DataObject;
+using IComDataObject = System.Runtime.InteropServices.ComTypes.IDataObject;
+using IFormsDataObject = System.Windows.Forms.IDataObject;
 
 namespace WindowsFormsClipboardRedux
 {
@@ -11,9 +12,26 @@ namespace WindowsFormsClipboardRedux
     {
         public static IFormsDataObject? GetDataObject()
         {
-            ClipboardImpl.Initialize();
+            if (Application.OleRequired() != ApartmentState.STA)
+            {
+                // Only throw if a message loop was started. This makes the case of trying
+                // to query the clipboard from your finalizer or non-ui MTA thread
+                // silently fail, instead of making your app die.
+                //
+                // however, if you are trying to write a normal windows forms app and
+                // forget to set the STAThread attribute, we will correctly report
+                // an error to aid in debugging.
+                if (Application.MessageLoop)
+                {
+                    throw new ThreadStateException("SR.ThreadMustBeSTA");
+                }
 
-            object dataObject = ClipboardImpl.Get();
+                return null;
+            }
+
+            // We need to retry the GetDataObject() since the clipBoard is busy sometimes and
+            // hence the GetDataObject would fail with ClipBoardException.
+            object dataObject = GetDataObject(retryTimes: 10, retryDelay: 100);
             if (dataObject is not null)
             {
                 if (dataObject is IFormsDataObject ido)
@@ -25,6 +43,57 @@ namespace WindowsFormsClipboardRedux
             }
 
             return null;
+        }
+
+        private static object GetDataObject(int retryTimes, int retryDelay)
+        {
+            IntPtr instance;
+            HRESULT hr;
+            int retry = retryTimes;
+            do
+            {
+                hr = Interops.Ole32.OleGetClipboard(out instance);
+                if (hr != HRESULT.S_OK)
+                {
+                    if (retry == 0)
+                    {
+                        hr.ThrowIfFailed();
+                    }
+
+                    retry--;
+                    Thread.Sleep(millisecondsTimeout: retryDelay);
+                }
+            }
+            while (hr != HRESULT.S_OK);
+
+            // Check if the returned value is actually a wrapped managed instance.
+            if (CCW_IDataObject.TryGetInstance(instance, out IFormsDataObject? forms_dataObject))
+            {
+                return forms_dataObject;
+            }
+
+            var iid = typeof(IComDataObject).GUID;
+            hr = Interops.Ole32.RoGetAgileReference(
+                Interops.Ole32.AgileReferenceOptions.Default,
+                ref iid,
+                instance,
+                out IntPtr agileReference);
+            if (hr != HRESULT.S_OK)
+            {
+                // Release the clipboard object if agile
+                // reference creation failed.
+                Marshal.Release(instance);
+                Marshal.ThrowExceptionForHR((int)hr);
+            }
+
+            // Wrap the agile reference.
+            var agileRef = new RCW_IAgileReference(agileReference);
+
+            // Release the current instance as it is now controlled
+            // by the agile reference RCW.
+            Marshal.Release(instance);
+
+            return new RCW_IDataObject(agileRef);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,33 +20,43 @@ namespace app15
             //var dataObject = Clipboard.GetDataObject();
 
             // Fails with: 'System.InvalidCastException: Specified cast is not valid.'
-            var dataObject1 = ClipboardRedux.GetDataObject();
-            if (dataObject1 is not null)
+
+            Console.WriteLine("Press ESC to stop");
+            do
             {
-                if (dataObject1.GetDataPresent(DataFormats.Bitmap))
+                while (!Console.KeyAvailable)
                 {
-                    var data = dataObject1.GetData(DataFormats.Bitmap);
-                    if (data is Bitmap bitmap)
+                    //var dataObject1 = Clipboard.GetDataObject();
+                    var dataObject1 = ClipboardRedux.GetDataObject();
+                    if (dataObject1 is not null)
                     {
-                        string path = Path.GetFullPath("clipboard.bmp");
-                        bitmap.Save(path);
-                        bitmap.Dispose();
-                        Debug.WriteLine($"Bitmap saved to {path}");
+                        if (dataObject1.GetDataPresent(DataFormats.Bitmap))
+                        {
+                            var data = dataObject1.GetData(DataFormats.Bitmap);
+                            if (data is Bitmap bitmap)
+                            {
+                                //string path = Path.GetFullPath("clipboard.bmp");
+                                //bitmap.Save(path);
+                                //bitmap.Dispose();
+                                //Debug.WriteLine($"Bitmap saved to {path}");
+                                Console.WriteLine("Is a bitmap");
+                            }
+                            else
+                            {
+                                Debug.WriteLine("Not a bitmap");
+                            }
+                        }
+                        else if (dataObject1.GetDataPresent(DataFormats.Text))
+                        {
+                            Console.WriteLine($"Text: {dataObject1.GetData(DataFormats.Text)}");
+                        }
                     }
                     else
                     {
-                        Debug.WriteLine("Not a bitmap");
+                        Console.WriteLine("opps");
                     }
                 }
-                else if (dataObject1.GetDataPresent(DataFormats.Text))
-                {
-                    Debug.WriteLine($"Text: {dataObject1.GetData(DataFormats.Text)}");
-                }
-            }
-            else
-            {
-                Debug.WriteLine("opps");
-            }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
